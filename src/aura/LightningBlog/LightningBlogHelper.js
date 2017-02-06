79c7a8d3a8d42106d860cc7c4d8ec55b695ea84c
Jeff Lombard <jeffl>
2017-02-05 18:18:02 -0800

    Apex Controller Working

--- src/aura/LightningBlog/LightningBlogHelper.js
+++ src/aura/LightningBlog/LightningBlogHelper.js
@@ -1,5 +1,26 @@
 ({
 	loadPostsHelper : function(component) {
-        return null;
+        var action = component.get("c.getBlogData");
+        var self = this;
+
+        action.setParams({
+            uuid: component.get("v.uuid")
+        });
+
+        action.setCallback(this, function(response){
+
+            var state = response.getState();
+
+            console.log('state: '+state);
+            console.log('response'+response);
+
+            alert(response.getReturnValue());
+
+            if(state === "SUCCESS"){
+            }
+        });
+        $A.enqueueAction(action);
+
+        console.log('action: '+action);
 	}
 })
